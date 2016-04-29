50f782509b67dda82321293d1f9740aac1a5321c
Xian Sun <xian_sun@brown.edu>
2016-04-28 20:44:41 -0400

    homepage

---
+++ javascript/homepage.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function(){
+    alert("here we do");
+     setInterval(linkedin, 3000);
+});
+
+function linkedin() {
+       if(IN.User.isAuthorized()) {
+            //TODO: XML request to server to render homepage
+       }
+
+ 
+}
